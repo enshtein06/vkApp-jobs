0fa0e34c5474f91b198762fd0b0283201bac9429
enshtein06 <enshtein06@gmail.com>
2019-05-28 23:36:12 +0300

    Updates

---
+++ precache-manifest.b29f04a967cca875e6ddf3626b5bb2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b4db2ed6d0f69b02fb9",
+    "url": "/vkApp-jobs/static/js/runtime~main.eb698806.js"
+  },
+  {
+    "revision": "68a96b96545a9158514b",
+    "url": "/vkApp-jobs/static/js/main.2705db48.chunk.js"
+  },
+  {
+    "revision": "8c43fcb68e1ecd53264f",
+    "url": "/vkApp-jobs/static/js/2.33f89bfb.chunk.js"
+  },
+  {
+    "revision": "68a96b96545a9158514b",
+    "url": "/vkApp-jobs/static/css/main.c3bfe118.chunk.css"
+  },
+  {
+    "revision": "8c43fcb68e1ecd53264f",
+    "url": "/vkApp-jobs/static/css/2.be9c4f48.chunk.css"
+  },
+  {
+    "revision": "6abe6e8b7a868089f08d7af510e30386",
+    "url": "/vkApp-jobs/index.html"
+  }
+];
