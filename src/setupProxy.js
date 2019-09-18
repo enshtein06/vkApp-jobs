925004ba70c015c6b18561771f9438680e6a36f9
enshtein06 <enshtein06@gmail.com>
2019-09-18 16:41:51 +0300

    test failde

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function setupProxy(app) {
   app.use(
     proxy("/api", {
-      target: "http://localhost:5001/",
+      target: "https://vk-jobs-application.herokuapp.com/",
       changeOrigin: true,
       secure: false,
       xfwd: true
